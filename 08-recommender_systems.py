02b617225428d6b4f00c3602093c3ece2d7d2a16
Senem <64867366+senemyalin@users.noreply.github.com>
2021-01-21 01:10:41 +0300

    Update 08-recommender_systems.py

--- 08-recommender_systems.py
+++ 08-recommender_systems.py
@@ -32,3 +32,4 @@ final_customer = last.apply(lambda row: row.fillna(row.mean()), axis=1)
 
 # customer similarity on replacing NAN by Customer(Reviewer) avg
 a = cosine_similarity(final_customer)
+np.fill_diagonal(a, 0 )
