63165232a1f60e2af6a24a2082c450eac269e266
Almira Gürkan <65222008+almiragurkan@users.noreply.github.com>
2021-01-21 00:46:32 +0300

    Update 03-data_brand_count.py

--- 03-data_brand_count.py
+++ 03-data_brand_count.py
@@ -37,3 +37,38 @@ for f, row in df.iterrows():
     brand=row['brand'] 
     if (count < 51 or count>5000):
         arr.append(brand)
+        
+for f, row in sorted_copy.iterrows():  
+    brand=row['brand']
+    
+    if (brand in arr):
+        sorted_copy=sorted_copy.drop(f,0)
+    
+        
+sorted_copy.to_pickle("pre_clustering1.pkl") #en üst ve en alt değerler çıkarıldı, ortalama değerler kaldı.
+
+
+import matplotlib.pyplot as plt
+last = pd.read_pickle('pre_clustering.pkl')
+
+#üst ve alt değerlerin çıkarılmamış hali
+count = last.groupby('brand').size()
+df = count.to_frame('count').reset_index()
+
+
+#üst ve alt değerlerin çıkarılmış hali
+last1 = pd.read_pickle('pre_clustering1.pkl')
+
+count1 = last1.groupby('brand').size()
+df1 = count1.to_frame('count').reset_index()    
+
+
+#plotting
+fig, axs = plt.subplots(1,2)
+fig.set_size_inches(12, 4)
+
+df.plot.scatter(x ='brand', y='count',ax=axs[0])
+df1.plot.scatter(x ='brand', y='count',ax=axs[1])
+
+plt.savefig("groupby_brand.pdf")
+plt.close(fig)
