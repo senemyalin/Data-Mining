4fd8924a73f06a10421d417bee86dc8ac531aec2
Almira Gürkan <65222008+almiragurkan@users.noreply.github.com>
2021-01-21 01:18:42 +0300

    Update 08-recommender_systems.py

--- 08-recommender_systems.py
+++ 08-recommender_systems.py
@@ -74,3 +74,4 @@ def customer_product_score(customer):
         avg_customer = Mean.loc[Mean['reviewerID'] == customer,'overall'].values[0]
         index = not_null.index.values.squeeze().tolist()
         correlated = similarity_with_product.loc[customer,index]
+        f = pd.concat([not_null, correlated], axis=1)
