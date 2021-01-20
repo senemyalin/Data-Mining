553d9480b093964244fbb80ee5ff4e98f0bbb228
Almira Gürkan <65222008+almiragurkan@users.noreply.github.com>
2021-01-21 00:58:28 +0300

    Update 07-MLPClassifier.py

--- 07-MLPClassifier.py
+++ 07-MLPClassifier.py
@@ -4,3 +4,5 @@ import numpy as np
 
 first = pd.read_pickle("customers_more_than_one_product.pkl")
 data = pd.read_pickle("customers_more_than_one_product.pkl")
+
+data['reviewerID'] = data["reviewerID"].rank(method='dense').astype(int)
