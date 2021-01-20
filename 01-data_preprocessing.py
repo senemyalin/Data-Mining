3a6e77f6ee67208b7b6cb82650375f8320f1ce3d
Senem <64867366+senemyalin@users.noreply.github.com>
2021-01-21 00:35:43 +0300

    Add files via upload

---
+++ 01-data_preprocessing.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+
+#Data preprocessing 
+meta_all_beauty = pd.read_json('C:\\Users\\senem\\Desktop\\meta_All_Beauty.json',typ="frame",lines=True)
+
+all_beauty = pd.read_json('C:\\Users\\senem\\Desktop\\All_Beauty.json',typ="frame",lines=True)
+
+all_beauty_copy = all_beauty.drop(['verified','reviewerName','summary','unixReviewTime',
+                                   'vote','style','image'], 1) #1 is for columns and 0 is for rows
+
+#pickle it for later use
+all_beauty_copy.to_pickle("all_beauty_second.pkl")
+
+
+meta_all_beauty_copy = meta_all_beauty.drop(['category','tech1','fit','description','also_buy'
+                                             ,'image','tech2','feature','rank','also_view','details'
+                                             ,'main_cat','similar_item','date','price'], 1)
+
+
+#if brand index is empty, this will remove that row 
+meta_all_beauty_copy_not_null=meta_all_beauty_copy
+
+
+
+for b, row in meta_all_beauty_copy.iterrows():  
+    i=row['brand'] 
+    if i == '':
+        meta_all_beauty_copy_not_null= meta_all_beauty_copy_not_null.drop(b,0)
+    
+        
+#pickle it for later use   
+meta_all_beauty_copy_not_null.to_pickle("meta_all_beauty_not_null.pkl")
+
+    
