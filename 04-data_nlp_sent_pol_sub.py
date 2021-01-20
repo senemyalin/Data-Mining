3de70ad545a18191737dc816c61a39c4c511c440
Senem <64867366+senemyalin@users.noreply.github.com>
2021-01-21 00:49:18 +0300

    Update 04-data_nlp_sent_pol_sub.py

--- 04-data_nlp_sent_pol_sub.py
+++ 04-data_nlp_sent_pol_sub.py
@@ -20,3 +20,33 @@ data['subjectivity'] = data['reviewText'].apply(sub)
 
 data.to_pickle("data_nlp_sent_pol_sub.pkl")
 
+
+#markaların polarity ve subjectivitylerini bularak 
+#sitede bulunan markaların aldıkları yorumların 
+#polarity ve subjectivitylerine baktık.
+
+data = pd.read_pickle('data_nlp_sent_pol_sub.pkl')
+
+pol=data.groupby('brand', as_index=False)['polarity'].mean()
+sub=data.groupby('brand', as_index=False)['subjectivity'].mean()
+
+brand_pol_sub = pd.merge(pol,sub, how= 'right', on = 'brand')
+
+####Her markanın polarity ve subjectivitylerinin ortalamarını yazdırdık.
+plt.figure()
+fig, ax= plt.subplots(figsize=(8,4))
+
+for index, brand in enumerate(brand_pol_sub.index):
+    
+    x = brand_pol_sub.loc[index,"polarity"]
+    y = brand_pol_sub.loc[index,"subjectivity"]
+    plt.scatter(x, y, color='blue')
+    plt.text(x+.001, y+.001, brand_pol_sub['brand'][index], fontsize=10)
+    
+    
+plt.title('Sentiment Analysis', fontsize=20)
+plt.xlabel('<-- Negative -------- Positive -->', fontsize=15)
+plt.ylabel('<-- Facts -------- Opinions -->', fontsize=15)
+
+plt.savefig("pol_sub_brand_scatter.pdf")
+plt.close(fig)
