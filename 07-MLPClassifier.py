a0f4ea33b20233fa8678d0244eb33b12b5c3f615
Almira Gürkan <65222008+almiragurkan@users.noreply.github.com>
2021-01-21 01:01:35 +0300

    Update 07-MLPClassifier.py

--- 07-MLPClassifier.py
+++ 07-MLPClassifier.py
@@ -19,5 +19,12 @@ X=np.vstack((X, x1,x2,x3)).T
 
 y= data["brand"].values[:15000]
 
-#Bir de ürünün bilgilerini öğretip kim almıştır gibi bir çıkarımda bulunmaya çalıştık
+#Ürünün bilgilerini öğretip kim almıştır gibi bir çıkarımda bulunmaya çalıştık
 #error rate 0,61 çıktı.
+
+#Ürün bilgilerini öğretip hangi brand olduğunu tahminlemeye çalıştık.
+#error rate 0.25 çıktı.
+
+#to split into train test sets
+from sklearn.model_selection import train_test_split
+X_train,X_test , y_train , y_test = train_test_split(X, y, test_size=0.3, random_state=1)
