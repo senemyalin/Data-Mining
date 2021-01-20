70601ade5eb7272eb6eedda23e16526f5cd3ed48
Senem <64867366+senemyalin@users.noreply.github.com>
2021-01-21 00:58:00 +0300

    Add files via upload

---
+++ 07-MLPClassifier.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import pandas as pd 
+import numpy as np
+
+first = pd.read_pickle("customers_more_than_one_product.pkl")
+data = pd.read_pickle("customers_more_than_one_product.pkl")
