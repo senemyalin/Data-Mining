582f06bbe456ff06a8b2969b00e7fcf6c058280e
Senem <64867366+senemyalin@users.noreply.github.com>
2021-01-21 00:58:44 +0300

    Update 07-MLPClassifier.py

--- 07-MLPClassifier.py
+++ 07-MLPClassifier.py
@@ -6,3 +6,4 @@ first = pd.read_pickle("customers_more_than_one_product.pkl")
 data = pd.read_pickle("customers_more_than_one_product.pkl")
 
 data['reviewerID'] = data["reviewerID"].rank(method='dense').astype(int)
+data['asin'] = data["asin"].rank(method='dense').astype(int)
